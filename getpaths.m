6ca9a38d27274400710ff1f8cd9a3d0690b38dcd
Todd Anderson <tshelbya@uw.edu>
2022-04-13 17:11:36 -0700

    update function forms

--- getpaths.m
+++ getpaths.m
@@ -1,4 +1,4 @@
-function getpaths(day, resolution)
+function pathlist_lite = getpaths(day, resolution)
 % getpaths.m
 % Todd Anderson
 % April 11, 2022
@@ -13,16 +13,19 @@ function getpaths(day, resolution)
 % lower bound on the length of the output file is 5*length(AP.data or
 % AP.power).
 %
-% NOTE: This script outputs 10-minute strokelist files, so that next steps
-% (e.g. pathGrid) can run with less memory use.
+% NOTE: This script writes 10-minute pathlist files, so that next steps
+% (e.g. pathgrid) can run with less memory use.  It outputs the full 1-day
+% pathlist, though, so 
 % 
+% MATLAB version compatibility
+%
 % INPUTS:
 %       day: day on which flare occurred (or for which stroke-station path
 %       file is needed)
-%       resolution: width of time bin, in minutes
+%       resolution: width of time bin, in minutes (default: 10)
 %
 % Requires:
-%       APfile(s)
+%       APfile for input day at /flash5/wd2/APfiles/[YEAR/]
 %       stations.mat
 %
 % OUTPUTS:
@@ -30,47 +33,48 @@ function getpaths(day, resolution)
 % 
 
 %% Input parameters:
+switch class(day)
+    case 'double'
+        if day > 7e5 && day < 8e5
+            daynum = day;
+        elseif day > 19900000 && day < 21000000
+            daynum = datenum(num2str(day), 'YYYYmmDD');
+        else
+            error('Cannot identify day format!')
+        end
+    case 'string'
+        daynum = datenum(day, 'YYYYmmDD');
+    otherwise
+        error('Cannot identify day format!');
+end
 
-% Enter start and stop APfile dates (inclusive).  For a single-day APfile,
-% startnum = stopnum.
-
-
-startnum = 20210507;
-stopnum = 20210507;
-
-starttime = datenum(2021,05,07,00,00,00);
-stoptime = datenum(2021,05,08,00,00,00);
-
-
+starttime = daynum;
+stoptime = daynum + 1;
 
 frames = 24*60/resolution;
 
-%% load AE, AP files
+%% load stations, AP file
 
-load('stations.mat');
+stations = importdata('stations.mat');
 
-flrAE       = [];
-flrAPdata   = [];
-flrAPpower  = [];
-% date range
-for k = startnum:stopnum
-    AEfilename = sprintf('AE%d.mat',k);
-    AEfile = importdata(AEfilename);
-    
-    % append current AE file to previous loop iteration's AE file
-    flrAE = cat(1,flrAE,AEfile);
-    
-    APfilename = sprintf('AP%d.mat',k);
-    APfile = importdata(APfilename);
-    data = APfile.data;
-    power = APfile.power;
-    
-    % append current AP.data, AP.power to previous loop iteration's AP data
-    % and power
-    flrAPdata = cat(1,flrAPdata,data);
-    flrAPpower = cat(2,flrAPpower,power);
-    
+daystring = datestr(daynum,'YYYYmmDD');
+year = datestr(daynum, 'YYYY');
+
+APfilename = sprintf('AP%s.mat',daystring);
+
+switch year
+    case {'2017','2018','2019'}
+        filepath = compose("/flash5/wd2/APfiles/%s/%s",year,APfilename);
+    case {'2020','2021','2022'}
+        filepath = compose("/flash5/wd2/APfiles/%s",APfilename);
+    otherwise
+        error('Input year outside range 2017-2022!')
 end
+
+fprintf('attempting import from path %s \n', filepath)
+APfile = importdata(filepath);
+data = APfile.data;
+power = APfile.power;
 
 %% (optional) UTC filter
 
@@ -80,20 +84,13 @@ end
 % flrAPdata = flrAPdata(utc_index,:);
 % flrAPpower = flrAPpower(:,utc_index);
 
-
-%% setup: load regional AE, AP files
-
-bigAE = flrAE;
-bigAPdata = flrAPdata;
-bigAPpower = flrAPpower;
-
 %% station sets: find stationIDs in AP.power a la detectDist.m
 
-strows = bigAPpower(1:2:end,:);
+strows = power(1:2:end,:);
 % convert stID from 0-start to 1-start, except station 0 (Dunedin)
 strows(strows > 0) = strows(strows > 0) + 1;
 nullrows = NaN.*ones(size(strows));
-stmat = ones(size(bigAPpower));
+stmat = ones(size(power));
 stmat(1:2:end,:) = strows;
 stmat(2:2:end,:) = nullrows;
 
@@ -103,21 +100,20 @@ stmatr1(stmatr1 == 0) = 1;
 stmat(1,:) = stmatr1;
 
 
-% Find every stationID; as of 4 Oct 2018 there are 122 entries in
+% Find every stationID; as of 11 April 2022 there are 127 entries in
 % stations.dat
 % 
 % Note that stations.dat, stations.mat etc begin with stID == 1, i.e.
 % stID == 1 corresponds to the Dunedin station
 
 % number of columns in stroke list = APdata (10) + station ID, lat, lon (3)
-strokelist = zeros(1,13);
+pathlist = zeros(1,13);
 for stID = 1:122
-    [row,col] = find(stmat == stID);   %all strokes detected by station number stID
-    lidx = sub2ind(size(bigAPpower),row + 1,col); %indices of strokes detected by stID in bigAPpower
+    [~,col] = find(stmat == stID);   %all strokes detected by station number stID
+    %lidx = sub2ind(size(power),row + 1,col); %indices of strokes detected by stID in AP.power
 
-    AE_stID = bigAE(col,:);
-    APdata_stID = bigAPdata(col,:);
-    APpower_stID = bigAPpower(lidx);
+    APdata_stID = data(col,:);
+    %APpower_stID = power(lidx);
     
     % return list of stroke time, location for each station
     % need dimension stID, or vectorize and concatenate
@@ -128,30 +124,30 @@ for stID = 1:122
     st_lat = stations{stID,1};
     st_lon = stations{stID,2};
     
-    strokelist_stID = cat(2,APdata_stID,stID*c,st_lat*c,st_lon*c);
+    pathlist_stID = cat(2,APdata_stID,stID*c,st_lat*c,st_lon*c);
     
-    strokelist = cat(1,strokelist,strokelist_stID);
+    pathlist = cat(1,pathlist,pathlist_stID);
     
 end
 
 %% Extract stroke-station path information: time, stroke lat/lon, station lat/lon
 
 % time
-stroke_time = datenum(strokelist(:,1:6));
+stroke_time = datenum(pathlist(:,1:6));
 
 % stroke lat/lon
-stroke_lat = strokelist(:,7);
-stroke_lon = strokelist(:,8);
+stroke_lat = pathlist(:,7);
+stroke_lon = pathlist(:,8);
 
 % station lat/lon
-stat_lat = strokelist(:,12);
-stat_lon = strokelist(:,13);
+stat_lat = pathlist(:,12);
+stat_lon = pathlist(:,13);
 
-strokelist_lite = cat(2,stroke_time,stroke_lat,stroke_lon,stat_lat,stat_lon);
+pathlist_lite = cat(2,stroke_time,stroke_lat,stroke_lon,stat_lat,stat_lon);
 
-rand_strokes = randperm(length(strokelist),10000);
+%rand_strokes = randperm(length(pathlist),10000);
 
-strokelist_10000 = strokelist_lite(rand_strokes,:);
+%pathlist_10000 = pathlist_lite(rand_strokes,:);
 
 
 %% Save path files
@@ -160,14 +156,16 @@ minute_bin_edges = linspace(starttime,stoptime,frames+1);
 
 for t = 1:frames
     
-    strokelist_lite_10m = strokelist_lite(strokelist_lite(:,1) >= minute_bin_edges(t) & strokelist_lite(:,1) < minute_bin_edges(t+1),:);
+    pathlist_lite_10m = pathlist_lite(pathlist_lite(:,1) >= minute_bin_edges(t) & pathlist_lite(:,1) < minute_bin_edges(t+1),:);
     
     filestr = datestr(minute_bin_edges(t),'yyyymmddHHMM');
-    filenum = str2double(filestr);
-    filename = sprintf('strokelist_lite_10m_%d.mat',filenum);
+    %filenum = str2double(filestr);
+    filename = sprintf('pathlist/pathlist_lite_10m_%s.mat',filestr);
     
-    save(filename,'strokelist_lite_10m');
+    save(filename,'pathlist_lite_10m');
             
+end
+
 end
 
 % save('strokelist.mat','strokelist');
