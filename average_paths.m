53f00bca6c417331373e9ad4fb5d19174b847db7
todd <andersontodds@gmail.com>
2022-06-26 16:48:39 -0700

    average ss paths in march 2022

--- average_paths.m
+++ average_paths.m
@@ -36,81 +36,110 @@ gc = importdata(gcfile);
 
 day_avg = mean(gc, 3, "omitnan");
 
-% %% 2. month
-% % average each lat, lon, UT element across 1 month
-% % requires grid_crossings_10 files for entire time range; either download
-% % these from flashlight or prepend "/gridstats" to gcfile below and run
-% % this part on flashlight
-% run_start = datenum(2022, 03, 01);
-% run_end = datenum(2022, 03, 31);
-% run_days = run_start:run_end;
-% run_days = run_days';
-% 
-% daystr = string(datestr(run_days, "yyyymmdd"));
-% 
-% gcfile = sprintf("grid_crossings_10m_%s.mat", daystr(1));
-% gc = importdata(gcfile);
-% gc_cavg = gc;
-% 
-% for j = 2:length(daystr)
-%     gcfile = sprintf("grid_crossings_10m_%s.mat", daystr(j));
-%     gc = importdata(gcfile);
-% 
-%     gc_cavg  = (gc_cavg.*(j-1) + gc)./j;
-% 
-% end
+%% 2. month
+% average each lat, lon, UT element across 1 month
+% requires grid_crossings_10 files for entire time range; either download
+% these from flashlight or prepend "/gridstats" to gcfile below and run
+% this part on flashlight
+run_start = datenum(2022, 03, 01);
+run_end = datenum(2022, 03, 31);
+run_days = run_start:run_end;
+run_days = run_days';
+
+daystr = string(datestr(run_days, "yyyymmdd"));
+
+% cumulative average method: avoid loading entire month of grid_crossings
+% at once
+% WARNING: any NaNs in first day will be propagated throughout whole
+% average!
+% load first day, initialize gc_avg
+gcfile = sprintf("data/grid_crossings_10m_%s.mat", daystr(1));
+gc = importdata(gcfile);
+gc_cavg = gc;
+
+% load subsequent days and calculate cumulative average
+for j = 2:length(daystr)
+    gcfile = sprintf("data/grid_crossings_10m_%s.mat", daystr(j));
+    gc = importdata(gcfile);
+
+    % NaN handling: set all NaNs in gc to current gc_cavg values for those
+    % array elements.
+    gc_nans = find(isnan(gc));
+    gc(gc_nans) = gc_cavg(gc_nans);
+
+    gc_cavg  = (gc_cavg.*(j-1) + gc)./j;
+
+end
 
 
 %% plot
-coastlines = importdata('coastlines.mat');
-coastlat = coastlines.coastlat;
-coastlon = coastlines.coastlon;
-geoidrefvec = [1,90,-180];
+% whole day average: plot day_avg
+% month average: plot gc_cavg(:,:,k); manually input desired frame k or
+% loop over k
+for k = 1:size(gc_cavg,3)
+    gplot = gc_cavg(:,:,k);
+    
+    times = linspace(run_start, run_start+1, 145);
+    timestring = string(datestr(times, "HH:MM:SS"));
+    
+    
+    coastlines = importdata('coastlines.mat');
+    coastlat = coastlines.coastlat;
+    coastlon = coastlines.coastlon;
+    geoidrefvec = [1,90,-180];
+    
+    figure(1)
+    hold off
+    t = tiledlayout(2,2, "TileSpacing","compact");
+    
+    nexttile([1,2])
+    worldmap("World")
+    geoshow(gplot, geoidrefvec, "DisplayType","texturemap");
+    hold on
+    geoshow(coastlat, coastlon, "Color","black");
+    set(gca,'ColorScale','log');
+    crameri('-hawaii');%,'pivot',1); % requires "crameri" colormap toolbox
+    
+    nexttile
+    %worldmap("World");
+    worldmap([60 90],[-180 180])
+    geoshow(gplot, geoidrefvec, "DisplayType","texturemap");
+    hold on
+    geoshow(coastlat, coastlon, "Color","black");
+    
+    xlabel("Latitude");
+    ylabel("Longitude");
+    title("");
+    
+    set(gca,'ColorScale','log');
+    crameri('-hawaii');%,'pivot',1); % requires "crameri" colormap toolbox
+    
+    
+    nexttile
+    worldmap([-90 -60],[-180 180])
+    geoshow(gplot, geoidrefvec, "DisplayType","texturemap");
+    hold on
+    geoshow(coastlat, coastlon, "Color","black");
+    
+    xlabel("Latitude");
+    ylabel("Longitude");
+    title("");
+    
+    set(gca,'ColorScale','log');
+    crameri('-hawaii');%,'pivot',1); % requires "crameri" colormap toolbox
+    cb = colorbar;
+    cb.Layout.Tile = 'east';
+    caxis([0.01 1000]);
+    
+    
+    titlestr = sprintf("Average number of WWLLN stroke-to-station path crossings \n March 2022 %s-%s", timestring(k), timestring(k+1));
+    title(t, titlestr);
+    %title(t, "Average number of WWLLN stroke-to-station path crossings in a 10 minute period, March 30, 2022");
 
-figure(1)
-hold off
-t = tiledlayout(2,2, "TileSpacing","compact");
+    if k == 1
+        gif('test3.gif');
+    else
+        gif;
+    end
 
-nexttile([1,2])
-worldmap("World")
-geoshow(day_avg, geoidrefvec, "DisplayType","texturemap");
-hold on
-geoshow(coastlat, coastlon, "Color","black");
-set(gca,'ColorScale','log');
-crameri('-hawaii');%,'pivot',1); % requires "crameri" colormap toolbox
-
-nexttile
-%worldmap("World");
-worldmap([60 90],[-180 180])
-geoshow(day_avg, geoidrefvec, "DisplayType","texturemap");
-hold on
-geoshow(coastlat, coastlon, "Color","black");
-
-xlabel("Latitude");
-ylabel("Longitude");
-title("");
-
-set(gca,'ColorScale','log');
-crameri('-hawaii');%,'pivot',1); % requires "crameri" colormap toolbox
-
-
-nexttile
-worldmap([-90 -60],[-180 180])
-geoshow(day_avg, geoidrefvec, "DisplayType","texturemap");
-hold on
-geoshow(coastlat, coastlon, "Color","black");
-
-xlabel("Latitude");
-ylabel("Longitude");
-title("");
-
-set(gca,'ColorScale','log');
-crameri('-hawaii');%,'pivot',1); % requires "crameri" colormap toolbox
-cb = colorbar;
-cb.Layout.Tile = 'east';
-caxis([0.01 1000]);
-
-
-
-
-title(t, "Average number of WWLLN stroke-to-station path crossings in a 10 minute period, March 30, 2022");
+end
