db467763ea8a78d4931d5f5c05b7389072421690
todd <andersontodds@gmail.com>
2022-09-01 17:34:50 -0700

    start avid network path comparison

---
+++ avid_paths.m
@@ -0,0 +1,23 @@
+% avid_paths.m
+% Todd Anderson
+% September 1 2022
+%
+% Plot Tx-Rx paths of proposed AVID network.  See Forrest Gasdia's
+% dissertation for diagram network.
+%
+% Tx:
+%   NLK
+%   NLM
+%   NAA
+%
+% Rx:
+%   Poker Flat
+%   Whitehorse
+%   Prince George
+%   Fort Nelson
+%   Yellowknife
+%   Rankin Inlet
+%   Iqaluit
+%   Kuujjuaq
+%   Labrador City
+%   Rimouski
