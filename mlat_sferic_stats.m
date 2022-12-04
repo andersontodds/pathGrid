1569b7bdf103feef502a8ded998f1c283f25189f
todd <andersontodds@gmail.com>
2022-12-03 16:59:17 -0800

    plot mean gtd in mlat bins for Nov 1-28

--- mlat_sferic_stats.m
+++ mlat_sferic_stats.m
@@ -8,49 +8,57 @@
 
 year = 2022;
 month = 11;
-day = 22;
+day = 01;
 
-gtdfile = sprintf("data/sferic_grouptimediff_gridcross_10m_%04g%02g%02g.mat", year, month, day);
-perpfile = sprintf("data/sferic_perp_gridcross_10m_%04g%02g%02g.mat", year, month, day);
-gcfile = sprintf("data/sferic_gridcrossings_10m_%04g%02g%02g.mat", year, month, day);
-gtd = importdata(gtdfile);
-perp = importdata(perpfile);
-gc = importdata(gcfile);
+% for day = 2:28
 
-gcpw = gc.*perp;
-gcpw_threshold = 1;
-
-mlatmesh = importdata("mlatmesh.mat");
-lsi = importdata("../landseaice/LSI_maskonly.mat");
-
-time_edge = linspace(datenum(year,month,day), datenum(year, month, day+1), size(gtd,3)+1);
-time_face = time_edge(2:end) - (time_edge(2) - time_edge(1));
-
-mlatrange = 50:70;
-
-colors = crameri('-lajolla', length(mlatrange)+2);
-colors = colors(2:end-1, :);
-
-figure(4)
-hold off
-
-gtd_mean = zeros(size(gtd, 3), length(mlatrange));
-for i = 1:length(mlatrange)
-    grid_in_bin = round(mlatmesh) == mlatrange(i);
-    for j = 1:size(gtd, 3)
-        gtd_frame = gtd(:,:,j);
-        gcpw_above_threshold = gcpw(:,:,j) > gcpw_threshold;
-        % mean gtd over mlat bin, accounting for different number of paths
-        % crossing each grid location
-        totalgc = sum(gc(grid_in_bin & gcpw_above_threshold));
-        gtd_mean(j,i) = sum(gtd_frame(grid_in_bin & gcpw_above_threshold).*gc(grid_in_bin & gcpw_above_threshold)/totalgc, "omitnan");
-        % mean of grid locations in mlat bin, not accounting for number of paths crossing each location    
-%         gtd_mean(j,i) = mean(gtd_frame(grid_in_bin & gcpw_above_threshold), "omitnan"); 
+    gtdfile = sprintf("data/sferic_grouptimediff_gridcross_10m_%04g%02g%02g.mat", year, month, day);
+    perpfile = sprintf("data/sferic_perp_gridcross_10m_%04g%02g%02g.mat", year, month, day);
+    gcfile = sprintf("data/sferic_gridcrossings_10m_%04g%02g%02g.mat", year, month, day);
+    gtd = importdata(gtdfile);
+    perp = importdata(perpfile);
+    gc = importdata(gcfile);
+    
+    gcpw = gc.*perp;
+    gcpw_threshold = 1;
+    
+    mlatmesh = importdata("mlatmesh.mat");
+    lsi = importdata("../landseaice/LSI_maskonly.mat");
+    
+    time_edge = linspace(datenum(year,month,day), datenum(year, month, day+1), size(gtd,3)+1);
+    time_face = time_edge(2:end) - (time_edge(2) - time_edge(1));
+    
+    mlatrange = 50:70;
+    
+    colors = crameri('-lajolla', length(mlatrange)+2);
+    colors = colors(2:end-1, :);
+    
+    figure(4)
+    hold off
+    
+    gtd_mean = zeros(size(gtd, 3), length(mlatrange));
+    for i = 1:length(mlatrange)
+        grid_in_bin = round(mlatmesh) == mlatrange(i);
+        for j = 1:size(gtd, 3)
+            gtd_frame = gtd(:,:,j);
+            gcpw_above_threshold = gcpw(:,:,j) > gcpw_threshold;
+            % mean gtd over mlat bin, accounting for different number of paths
+            % crossing each grid location
+            totalgc = sum(gc(grid_in_bin & gcpw_above_threshold));
+            gtd_mean(j,i) = sum(gtd_frame(grid_in_bin & gcpw_above_threshold).*gc(grid_in_bin & gcpw_above_threshold)/totalgc, "omitnan");
+            % mean of grid locations in mlat bin, not accounting for number of paths crossing each location    
+    %         gtd_mean(j,i) = mean(gtd_frame(grid_in_bin & gcpw_above_threshold), "omitnan"); 
+        end
+    
+        plot(datetime(time_face, "ConvertFrom","datenum"), gtd_mean(:,i), '-', "Color", colors(i,:))
+        hold on
     end
+    
+    ylabel("\omega_0^2/2c (s m^{-1})")
+    ylim([0.025 0.125])
+    title("average \omega_0^2/2c at different magnetic latitudes")
+    
+    figname = sprintf("figures/mean_gtd_mlat_%04g%02g%02g.jpg", year, month, day);
+    saveas(gcf, figname);
 
-    plot(datetime(time_face, "ConvertFrom","datenum"), gtd_mean(:,i), '-', "Color", colors(i,:))
-    hold on
-end
-
-ylabel("c3/d")
-ylim([0.025 0.125])
+% end
